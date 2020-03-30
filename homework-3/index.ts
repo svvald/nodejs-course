e5f75771e56d2722556f85ccfa9ee00057dc1f3b
Nikita Chistousov <chistousov.nd@gmail.com>
2020-03-31 00:38:36 +0300

    (HW-7) - Set up testing environment and covered User controller methods by tests

--- homework-3/index.ts
+++ homework-3/index.ts
@@ -1,16 +1,11 @@
-import 'reflect-metadata';
-
-import express from 'express';
-
-import initLoaders from './loaders';
+import initEnvironment from './loaders';
 import config from './config/environment';
+import server from './loaders/express';
 
 async function startServer(): Promise<void> {
-  const app = express();
+  await initEnvironment();
 
-  await initLoaders(app);
-
-  app.listen(config.app.port, () => {
+  server.listen(config.app.port, () => {
     console.log(`Server is ready and listening on port ${config.app.port}`);
   });
 }
